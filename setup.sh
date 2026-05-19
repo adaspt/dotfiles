81e1715ec9c88ae9b3fbfaa0e678069edf17fcc8
Adas Petrovas <adas.petrovas@gmail.com>
2026-05-19 16:14:05 +0300

    Improvements

--- setup.sh
+++ setup.sh
@@ -7,6 +7,14 @@ FILES=(
   "Backup/agersi-vpn.conf.age"
   "Backup/ssh.tar.gz.age"
 )
+TMP_FILES=()
+
+cleanup() {
+  if [ "${#TMP_FILES[@]}" -gt 0 ]; then
+    rm -f "${TMP_FILES[@]}"
+  fi
+}
+trap cleanup EXIT
 
 sudo dnf install -y rclone age zsh
 
@@ -43,31 +51,35 @@ done
 mkdir -p "$HOME/.ssh"
 if [ -f "$DEST_DIR/Backup/ssh.tar.gz.age" ]; then
   echo "Setting up SSH keys"
-  age --decrypt "$DEST_DIR/Backup/ssh.tar.gz.age" > "$DEST_DIR/Backup/ssh.tar.gz"
-  tar -xzf "$DEST_DIR/Backup/ssh.tar.gz" -C "$HOME/.ssh"
-  rm "$DEST_DIR/Backup/ssh.tar.gz"
+  ssh_archive="$(mktemp)"
+  TMP_FILES+=("$ssh_archive")
+  chmod 600 "$ssh_archive"
+  age --decrypt "$DEST_DIR/Backup/ssh.tar.gz.age" > "$ssh_archive"
+  tar -xzf "$ssh_archive" -C "$HOME/.ssh"
 fi
 
 chmod 700 "$HOME/.ssh"
-chmod 600 ~/.ssh/id_*
-chmod 644 ~/.ssh/*.pub
-chmod 600 ~/.ssh/config
-chmod 600 ~/.ssh/authorized_keys
-chmod 644 ~/.ssh/known_hosts
+find "$HOME/.ssh" -maxdepth 1 -type f -name "id_*" ! -name "*.pub" -exec chmod 600 {} +
+find "$HOME/.ssh" -maxdepth 1 -type f -name "*.pub" -exec chmod 644 {} +
+[ -f "$HOME/.ssh/config" ] && chmod 600 "$HOME/.ssh/config"
+[ -f "$HOME/.ssh/authorized_keys" ] && chmod 600 "$HOME/.ssh/authorized_keys"
+[ -f "$HOME/.ssh/known_hosts" ] && chmod 644 "$HOME/.ssh/known_hosts"
 
 # ---------- VPN ----------
 if ! nmcli connection show agersi-vpn &> /dev/null; then
   echo "Setting up VPN connection"
-  age --decrypt "$DEST_DIR/Backup/agersi-vpn.conf.age" > "$DEST_DIR/Backup/agersi-vpn.conf"
-  nmcli connection import type wireguard file "$DEST_DIR/Backup/agersi-vpn.conf"
+  vpn_config="$(mktemp)"
+  TMP_FILES+=("$vpn_config")
+  chmod 600 "$vpn_config"
+  age --decrypt "$DEST_DIR/Backup/agersi-vpn.conf.age" > "$vpn_config"
+  nmcli connection import type wireguard file "$vpn_config"
   nmcli connection modify agersi-vpn connection.autoconnect no
-  rm "$DEST_DIR/Backup/agersi-vpn.conf"
 fi
 
 # ---------- Resources ----------
 echo "Setting up resources"
 DOTFILES_DIR="$HOME/Projects/personal/dotfiles"
-mkdir -p "$DOTFILES_DIR"
+mkdir -p "$(dirname "$DOTFILES_DIR")"
 if [ ! -d "$DOTFILES_DIR/.git" ]; then
   git clone git@github.com:adaspt/dotfiles.git "$DOTFILES_DIR"
 else
@@ -79,13 +91,21 @@ echo "Setting up shell and configuration"
 chsh -s "$(which zsh)"
 cp -r "$DOTFILES_DIR/config"/* "$HOME/"
 mkdir -p "$HOME/.zsh"
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git "$HOME/.zsh/fast-syntax-highlighting"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git "$HOME/.zsh/zsh-autosuggestions"
+if [ ! -d "$HOME/.zsh/fast-syntax-highlighting/.git" ]; then
+  git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git "$HOME/.zsh/fast-syntax-highlighting"
+else
+  git -C "$HOME/.zsh/fast-syntax-highlighting" pull
+fi
+if [ ! -d "$HOME/.zsh/zsh-autosuggestions/.git" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git "$HOME/.zsh/zsh-autosuggestions"
+else
+  git -C "$HOME/.zsh/zsh-autosuggestions" pull
+fi
 
 # ---------- Fonts ----------
 echo "Setting up fonts"
 mkdir -p "$HOME/.local/share/fonts"
-cp -r "$DOTFILES_DIR/fonts/*" "$HOME/.local/share/fonts"
+cp -r "$DOTFILES_DIR/fonts"/* "$HOME/.local/share/fonts"
 fc-cache -fv
 
 # ---------- Yazi ----------
@@ -105,8 +125,19 @@ BASE_PATH="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/grad
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$BASE_PATH name "Gradia Screenshot"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$BASE_PATH command "flatpak run be.alexandervanhee.gradia --screenshot=INTERACTIVE"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$BASE_PATH binding "<Super><Shift>s"
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['$BASE_PATH']"
+CURRENT_KEYBINDINGS="$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings)"
+if [[ "$CURRENT_KEYBINDINGS" != *"'$BASE_PATH'"* && "$CURRENT_KEYBINDINGS" != *"\"$BASE_PATH\""* ]]; then
+  if [[ "$CURRENT_KEYBINDINGS" == "@as []" || "$CURRENT_KEYBINDINGS" == "[]" ]]; then
+    gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['$BASE_PATH']"
+  else
+    gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "${CURRENT_KEYBINDINGS%]}, '$BASE_PATH']"
+  fi
+fi
 
 # ---------- Gnome extensions ----------
-echo "Setting up Gnome extensions"
-gnome-extensions install-extension 307 # dash-to-dock
+# TODO: Investigate using gnome-browser-connector gnome-extensions://caffeine%40patapon.info/?action=install
+# echo "Setting up Gnome extensions"
+# if command -v gnome-extensions &> /dev/null && ! gnome-extensions list | grep -qx "dash-to-dock@micxgx.gmail.com"; then
+#  sudo dnf install -y gnome-shell-extension-dash-to-dock || echo "Could not install dash-to-dock"
+#  gnome-extensions enable dash-to-dock@micxgx.gmail.com || echo "Could not enable dash-to-dock"
+# fi
