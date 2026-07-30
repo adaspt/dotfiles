3a1e8aecc99f9388aa055a430302e17e200e91fd
Adas Petrovas <adas.petrovas@gmail.com>
2026-07-30 11:15:38 +0300

    Azure CLI

--- setup-development.sh
+++ setup-development.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 DOTFILES_DIR="$HOME/Projects/personal/dotfiles"
 
-sudo pacman -S --needed --noconfirm nvm
+sudo pacman -S --needed --noconfirm nvm azure-cli
 yay -S --noconfirm dotnet-runtime-10.0 dotnet-sdk-10.0 aspnet-runtime-10.0 aspnet-targeting-pack-10.0
 
 yay -S --noconfirm claude-code claude-desktop
